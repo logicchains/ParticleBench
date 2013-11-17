7ef5632b5e5d35da71f338c1c87cb340ce5bda7c
Daniel Hams <daniel.hams@gmail.com>
2013-11-17 16:47:32 +0000

    Have eliminated all of the global mutable variables coming from the C version. Still some work to do to see what else can be "C++ified".

--- CPP.cpp
+++ CPP.cpp
@@ -48,33 +48,17 @@ constexpr int32_t MAX_PTS = (RUNNING_TIME * POINTS_PER_SEC);
 constexpr uint32_t NUM_VERTICES = 24;
 constexpr uint32_t NUM_NORMALS = NUM_VERTICES / 4;
 
-double initT = 0;
-double endT = 0;
-double frameDur = 0;
-double spwnTmr = 0;
-double cleanupTmr = 0;
-double runTmr = 0;
-
-double frames[RUNNING_TIME * 1000];
-uint64_t curFrame = 0;
+constexpr uint32_t RAND_SEED = 1234569;
 
 struct Pt {
 	double X; double Y; double Z; double VX; double VY; double VZ; double R; double Life; 
 	bool is;
 };
-struct Pt Pts[MAX_PTS];
-int numPts = 0;      
-int minPt = 0;       
-uint32_t seed = 1234569;
 
 struct Vertex {
 	GLfloat pos[3];
 	GLfloat normal[3];
 };
-
-struct Vertex Vertices[ NUM_VERTICES ];
-uint32_t curVertex = 0;
-uint32_t curNormal = 0;
 
 const GLfloat srcCoords[ NUM_VERTICES ][3] =
 {
@@ -119,27 +103,6 @@ const GLfloat srcNormals[ NUM_NORMALS ][3] =
   {-1, 0, 0}
 };
 
-void newVertex( int x, int y, int z){
-	struct Vertex thisVertex;
-	thisVertex.pos[0] = x;
-	thisVertex.pos[1] = y;
-	thisVertex.pos[2] = z;
-	Vertices[curVertex] = thisVertex;
-	curVertex++;
-}
-
-void newNormal( int nx, int ny, int nz){
-	for (int i = curNormal*4; i < (curNormal+1)*4; i++){
-		Vertices[i].normal[0] = nx;
-		Vertices[i].normal[1] = ny;
-		Vertices[i].normal[2] = nz;
-	}
-	curNormal++;	
-}
-
-
-GLuint gVBO = 0;
-
 double windX = 0; 
 double windY = 0;
 double windZ = 0;
@@ -166,62 +129,206 @@ struct XorRandGenerator
 };
 
 template<class RandGenerator>
+class Particles
+{
+public:
+  Particles( RandGenerator & randGenerator, const uint32_t numParticles ) :
+    randGenerator_( randGenerator ),
+    numPts( 0 ),
+    minPt( 0 ),
+    particles_( numParticles, Pt {0, 0, 0, 0, 0, 0, 0, 0, 0 } )
+  {
+  };
+
+  void moveParticles(double secs)
+  {
+    uint32_t newMinPt( minPt );
+    bool onExpired( true );
+    for( uint32_t i = minPt; i < numPts; i++)
+    {
+      Pt & p( particles_[i] );
+      if( p.is == false )
+      {
+	continue;
+      }
+      p.X += p.VX * secs;
+      p.Y += p.VY * secs;
+      p.Z += p.VZ * secs;
+      p.VX += windX * 1 / p.R;
+      p.VY += windY * 1 / p.R;
+      p.VY -= grav;
+      p.VZ += windZ * 1 / p.R;
+      p.Life -= secs;
+      if (p.Life <= 0 )
+      {
+	p.is = false;
+      }
+    }
+  }
+
+  void spawnParticles(double secs, uint32_t & randValue )
+  {
+    uint32_t num = secs * POINTS_PER_SEC;
+    uint32_t i = 0;
+    for (; i < num; i++)
+      {
+	Pt & pt = particles_[numPts];
+	pt.X = 0 + (double)( randGenerator_.mod( randValue, START_RANGE ) ) - START_RANGE/2;
+	pt.Y = START_Y;
+	pt.Z = START_DEPTH + (double)( randGenerator_.mod( randValue, START_RANGE ) ) - START_RANGE/2;
+	pt.VX = (double)( randGenerator_.mod( randValue, MAX_INIT_VEL) );
+	pt.VY = (double)( randGenerator_.mod( randValue, MAX_INIT_VEL) );
+	pt.VZ = (double)( randGenerator_.mod( randValue, MAX_INIT_VEL) );
+	pt.R = (double)( randGenerator_.mod( randValue, (MAX_SCALE*100) ) ) / 200;
+	pt.Life = (double)( randGenerator_.mod( randValue, MAX_LIFE) ) / 1000;
+	pt.is = true;
+	numPts++;
+      }
+  }
+
+  void checkForCollisions()
+  {
+    for (int i = minPt; i < numPts; i++)
+      {
+	Pt & p( particles_[i] );
+	if (p.is == false)
+	  {
+	    continue;
+	  }
+	if (p.X < MIN_X)
+	  {
+	    p.X = MIN_X + p.R;
+	    p.VX *= -1.1; // These particles are magic; they accelerate by 10% at every bounce off the bounding box
+	  }
+	if (p.X > MAX_X)
+	  {
+	    p.X = MAX_X - p.R;
+	    p.VX *= -1.1;
+	  }
+	if (p.Y < MIN_Y)
+	  {
+	    p.Y = MIN_Y + p.R;
+	    p.VY *= -1.1;
+	  }
+	if (p.Y > MAX_Y)
+	  {
+	    p.Y = MAX_Y - p.R;
+	    p.VY *= -1.1;
+	  }
+	if (p.Z < MIN_DEPTH)
+	  {
+	    p.Z = MIN_DEPTH + p.R;
+	    p.VZ *= -1.1;
+	  }
+	if (p.Z > MAX_DEPTH)
+	  {
+	    p.Z = MAX_DEPTH - p.R;
+	    p.VZ *= -1.1;
+	  }
+      }
+  }
+
+  void renderParticles()
+  {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    for (int i = minPt; i < numPts; i++)
+      {
+	Pt & p( particles_[i] );
+	if (p.is == false)
+	  {
+	    continue;
+	  }
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(p.X, p.Y, -p.Z);
+	glScalef(p.R*2, p.R*2, p.R*2);
+	glColor4f(0.7, 0.9, 0.2, 1);
+	glDrawArrays( GL_QUADS, 0, 24 );	
+      }
+  }
+
+  void cleanupPtPool()
+  {
+    for (int i = minPt; i < numPts; i++)
+    {
+      Pt & p( particles_[ i ]);
+      if (p.is == true)
+      {
+	minPt = i;
+	break;
+      }
+    }
+  }
+
+private:
+  int numPts;
+  int minPt;
+  vector<Pt> particles_;
+  RandGenerator & randGenerator_;
+};
+
+template<class RandGenerator>
 class GLRenderer
 {
 public:
   GLRenderer( uint32_t randSeed ) :
     randValue_( randSeed ),
-    points_( MAX_PTS, Pt() )
+    gVBO_( 0 ),
+    particles_( randGenerator_, MAX_PTS ),
+    spwnTmr_( 0.0 ),
+    cleanupTmr_( 0.0 )
   {
     vertices_.reserve( NUM_VERTICES );
   }
 
   void initScene()
   {
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
 
-	glClearColor(0.1, 0.1, 0.6, 1.0);
-	glClearDepth(1);
-	glDepthFunc(GL_LEQUAL);
+    glClearColor(0.1, 0.1, 0.6, 1.0);
+    glClearDepth(1);
+    glDepthFunc(GL_LEQUAL);
 
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-	glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+    glEnable(GL_LIGHT0);
 
-	glViewport(0, 0, WIDTH, HEIGHT);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-1, 1, -1, 1, 1.0, 1000.0);
-	glRotatef(20, 1, 0, 0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glPushMatrix();
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1, 1, -1, 1, 1.0, 1000.0);
+    glRotatef(20, 1, 0, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();
   }
 
   void setupBuffers()
   {
-	uint32_t curCoord { 0 };
-	for( uint32_t n = 0 ; n < NUM_NORMALS ; ++n )
-	{
-	  const GLfloat *cn = srcNormals[n];
-	  for( uint32_t p = 0 ; p < 4 ; ++p, ++curCoord )
-	  {
-	    const GLfloat *cv = srcCoords[curCoord];
-	    vertices_.emplace_back( Vertex {{cv[0], cv[1], cv[2] }, {cn[0], cn[1], cn[2]}} );
-	  }
-	}
+    uint32_t curCoord { 0 };
+    for( uint32_t n = 0 ; n < NUM_NORMALS ; ++n )
+    {
+      const GLfloat *cn = srcNormals[n];
+      for( uint32_t p = 0 ; p < 4 ; ++p, ++curCoord )
+      {
+	const GLfloat *cv = srcCoords[curCoord];
+	vertices_.emplace_back( Vertex {{cv[0], cv[1], cv[2] }, {cn[0], cn[1], cn[2]}} );
+      }
+    }
 
-	glGenBuffers( 1, &gVBO );
-	glBindBuffer( GL_ARRAY_BUFFER, gVBO );
-	glBufferData( GL_ARRAY_BUFFER, NUM_VERTICES * sizeof(Vertex), &(vertices_[0]), GL_STATIC_DRAW );
+    glGenBuffers( 1, &gVBO_ );
+    glBindBuffer( GL_ARRAY_BUFFER, gVBO_ );
+    glBufferData( GL_ARRAY_BUFFER, NUM_VERTICES * sizeof(Vertex), &(vertices_[0]), GL_STATIC_DRAW );
 
-	glEnableClientState( GL_VERTEX_ARRAY );
-	glEnableClientState( GL_NORMAL_ARRAY );	
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glEnableClientState( GL_NORMAL_ARRAY );	
 
-	glVertexPointer( 3, GL_FLOAT, sizeof( Vertex), 0 );
-	glNormalPointer( GL_FLOAT, sizeof( Vertex ), (const GLvoid *)offsetof( Vertex, normal ) );
+    glVertexPointer( 3, GL_FLOAT, sizeof( Vertex), 0 );
+    glNormalPointer( GL_FLOAT, sizeof( Vertex ), (const GLvoid *)offsetof( Vertex, normal ) );
   }
 
   void teardownBuffers()
@@ -229,32 +336,15 @@ public:
     glDisableClientState( GL_NORMAL_ARRAY );
     glDisableClientState( GL_VERTEX_ARRAY );
 
-    glDeleteBuffers( 1, &gVBO );
+    glDeleteBuffers( 1, &gVBO_ );
   }
 
-  void movePts(double secs)
+  void moveParticles(double secs)
   {
-    for (int i = minPt; i <= numPts; i++)
-    {
-      if (Pts[i].is == false)
-      {
-	continue;
-      }
-      Pts[i].X += Pts[i].VX * secs;
-      Pts[i].Y += Pts[i].VY * secs;
-      Pts[i].Z += Pts[i].VZ * secs;
-      Pts[i].VX += windX * 1 / Pts[i].R;
-      Pts[i].VY += windY * 1 / Pts[i].R;
-      Pts[i].VY -= grav;
-      Pts[i].VZ += windZ * 1 / Pts[i].R;
-      Pts[i].Life -= secs;
-      if (Pts[i].Life <= 0) {
-	Pts[i].is = false;
-      }
-    }
+    particles_.moveParticles( secs );
   }
 
-  void doWind()
+  void doWind( const double frameDur )
   {
     windX += ( (double)( randGenerator_.mod( randValue_, WIND_CHANGE ) ) / WIND_CHANGE - WIND_CHANGE/2000) * frameDur;
     windY += ( (double)( randGenerator_.mod( randValue_, WIND_CHANGE ) ) / WIND_CHANGE - WIND_CHANGE/2000) * frameDur;
@@ -273,86 +363,48 @@ public:
       }
   }
 
-  void spawnPts(double secs)
+  void spawnParticles(double secs)
   {
-    uint32_t num = secs * POINTS_PER_SEC;
-    uint32_t i = 0;
-    for (; i < num; i++)
-      {
-	struct Pt pt;
-	pt.X = 0 + (double)( randGenerator_.mod( randValue_, START_RANGE ) ) - START_RANGE/2;
-	pt.Y = START_Y;
-	pt.Z = START_DEPTH + (double)( randGenerator_.mod( randValue_, START_RANGE ) ) - START_RANGE/2;
-	pt.VX = (double)( randGenerator_.mod( randValue_, MAX_INIT_VEL) );
-	pt.VY = (double)( randGenerator_.mod( randValue_, MAX_INIT_VEL) );
-	pt.VZ = (double)( randGenerator_.mod( randValue_, MAX_INIT_VEL) );
-	pt.R = (double)( randGenerator_.mod( randValue_, (MAX_SCALE*100) ) ) / 200;
-	pt.Life = (double)( randGenerator_.mod( randValue_, MAX_LIFE) ) / 1000;
-	pt.is = true;
-	Pts[numPts] = pt;
-	numPts++;
-      }
+    particles_.spawnParticles( secs, randValue_ );
   }
 
   void checkColls()
   {
-    for (int i = minPt; i <= numPts; i++)
-      {
-	if (Pts[i].is == false)
-	  {
-	    continue;
-	  }
-	if (Pts[i].X < MIN_X)
-	  {
-	    Pts[i].X = MIN_X + Pts[i].R;
-	    Pts[i].VX *= -1.1; // These particles are magic; they accelerate by 10% at every bounce off the bounding box
-	  }
-	if (Pts[i].X > MAX_X)
-	  {
-	    Pts[i].X = MAX_X - Pts[i].R;
-	    Pts[i].VX *= -1.1;
-	  }
-	if (Pts[i].Y < MIN_Y)
-	  {
-	    Pts[i].Y = MIN_Y + Pts[i].R;
-	    Pts[i].VY *= -1.1;
-	  }
-	if (Pts[i].Y > MAX_Y)
-	  {
-	    Pts[i].Y = MAX_Y - Pts[i].R;
-	    Pts[i].VY *= -1.1;
-	  }
-	if (Pts[i].Z < MIN_DEPTH)
-	  {
-	    Pts[i].Z = MIN_DEPTH + Pts[i].R;
-	    Pts[i].VZ *= -1.1;
-	  }
-	if (Pts[i].Z > MAX_DEPTH)
-	  {
-	    Pts[i].Z = MAX_DEPTH - Pts[i].R;
-	    Pts[i].VZ *= -1.1;
-	  }
-      }
+    particles_.checkForCollisions();
   }
 
   void renderPts()
   {
+    particles_.renderParticles();
+  }
 
-    for (int i = minPt; i <= numPts; i++)
-      {
-	if (Pts[i].is == false)
-	  {
-	    continue;
-	  }
-	struct Pt *pt = &Pts[i];
-	glMatrixMode(GL_MODELVIEW);
-	glPopMatrix();
-	glPushMatrix();
-	glTranslatef(pt->X, pt->Y, -pt->Z);
-	glScalef(pt->R * 2, pt->R*2, pt->R*2);
-	glColor4f(0.7, 0.9, 0.2, 1);
-	glDrawArrays( GL_QUADS, 0, 24 );	
-      }
+  void cleanupPtPool()
+  {
+    particles_.cleanupPtPool();
+  }
+
+  void doTimestep( const double frameDuration )
+  {
+    moveParticles( frameDuration );
+    doWind( frameDuration );
+    if( spwnTmr_ >= SPAWN_INTERVAL )
+    {
+      spawnParticles( SPAWN_INTERVAL );
+      spwnTmr_ -= SPAWN_INTERVAL;
+    }
+    if( cleanupTmr_ >= (MAX_LIFE/1000.0) )
+    {
+      cleanupPtPool();
+      cleanupTmr_ = 0;
+    }
+    checkColls();
+    renderPts();
+  }
+
+  void updateTimers( const double frameDuration )
+  {
+    spwnTmr_ += frameDuration;
+    cleanupTmr_ += frameDuration;
   }
 
   ~GLRenderer()
@@ -363,102 +415,94 @@ private:
   RandGenerator randGenerator_;
   uint32_t randValue_;
   vector<Vertex> vertices_;
-  vector<Pt> points_;
+
+  GLuint gVBO_;
+
+  Particles<RandGenerator> particles_;
+
+  double spwnTmr_;
+  double cleanupTmr_;
 };
 
 
-void cleanupPtPool() {
-	for (int i = minPt; i <= numPts; i++) {
-		if (Pts[i].is == true) {
-			minPt = i;
-			break;
-		}
-	}
-}
-
 void error_callback(int error, const char* description){
 	fputs(description, stderr);
+	fflush( stderr );
 }
 
 int main(int argc, char* argv[]) {
-	glfwSetErrorCallback(error_callback);
-	if( !glfwInit() ){
-		exit(EXIT_FAILURE);
-	}
-	glfwWindowHint(GLFW_SAMPLES, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL); 
-	if (!window){
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(0);
+  vector<double> frames( (RUNNING_TIME * 1000), 0.0 );
+  uint64_t curFrame( 0 );
+  glfwSetErrorCallback(error_callback);
+  if( !glfwInit() ){
+    exit(EXIT_FAILURE);
+  }
+  glfwWindowHint(GLFW_SAMPLES, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL); 
+  if (!window){
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(0);
 
-	GLRenderer<XorRandGenerator> glRenderer( seed );
-	glRenderer.initScene();
+  GLRenderer<XorRandGenerator> glRenderer( RAND_SEED );
+  glRenderer.initScene();
 
-	GLenum glewError = glewInit();
-	if( glewError != GLEW_OK ){
-		printf( "Error initializing GLEW! %s\n", glewGetErrorString( glewError ) );
-		return false;
-	}
-	if( !GLEW_VERSION_2_1 ){
-		printf( "OpenGL 2.1 not supported!\n" );
-		return false;
-	}
-	glRenderer.setupBuffers();
-	while (!glfwWindowShouldClose(window)){
-		initT = glfwGetTime();
-		glRenderer.movePts(frameDur);
-		glRenderer.doWind();
-		if (spwnTmr >= SPAWN_INTERVAL) {
-			glRenderer.spawnPts(SPAWN_INTERVAL);
-			spwnTmr -= SPAWN_INTERVAL;
-		}
-		if (cleanupTmr >= (double)MAX_LIFE/1000) {
-			cleanupPtPool();
-			cleanupTmr = 0;
-		}
-		glRenderer.checkColls();
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  GLenum glewError = glewInit();
+  if( glewError != GLEW_OK ){
+    printf( "Error initializing GLEW! %s\n", glewGetErrorString( glewError ) );
+    return false;
+  }
+  if( !GLEW_VERSION_2_1 ){
+    printf( "OpenGL 2.1 not supported!\n" );
+    return false;
+  }
 
-		glRenderer.renderPts();
+  glRenderer.setupBuffers();
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+  double initT, endT, frameDur, runTmr = 0;
 
-		endT = glfwGetTime();
-		frameDur = endT-initT; 
-		spwnTmr += frameDur;
-		cleanupTmr += frameDur;
-		runTmr += frameDur;
-		if (runTmr > MAX_LIFE/1000) { 
-			frames[curFrame] = frameDur;
-			curFrame += 1;			
-		}
+  while (!glfwWindowShouldClose(window)){
+    initT = glfwGetTime();
+
+    glRenderer.doTimestep( frameDur );
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    endT = glfwGetTime();
+    frameDur = endT-initT;
+    glRenderer.updateTimers( frameDur );
+
+    runTmr += frameDur;
+    if (runTmr > MAX_LIFE/1000) { 
+      frames[curFrame] = frameDur;
+      curFrame += 1;			
+    }
 		
-		if (runTmr >= RUNNING_TIME) {
-			double sum = 0;
-			uint64_t i = 0;
-			for (i = 0; i < curFrame; i++) {
-				sum += frames[i];
-			}
-			double mean = sum / (double)curFrame;
-			printf("Average framerate was: %f frames per second.\n", 1/mean);		
-			double sumDiffs = 0.0;
-			for (i = 0; i < curFrame; i++) {
-				sumDiffs += pow((1/frames[i])-(1/mean), 2);
-			}
-			double variance = sumDiffs/ (double)curFrame;
-			double sd = sqrt(variance);
-			printf("The standard deviation was: %f frames per second.\n", sd);
-			break;
-		}
-	}
-	glRenderer.teardownBuffers();
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+    if (runTmr >= RUNNING_TIME) {
+      double sum = 0;
+      uint64_t i = 0;
+      for (i = 0; i < curFrame; i++) {
+	sum += frames[i];
+      }
+      double mean = sum / (double)curFrame;
+      printf("Average framerate was: %f frames per second.\n", 1/mean);		
+      double sumDiffs = 0.0;
+      for (i = 0; i < curFrame; i++) {
+	sumDiffs += pow((1/frames[i])-(1/mean), 2);
+      }
+      double variance = sumDiffs/ (double)curFrame;
+      double sd = sqrt(variance);
+      printf("The standard deviation was: %f frames per second.\n", sd);
+      break;
+    }
+  }
+  glRenderer.teardownBuffers();
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
